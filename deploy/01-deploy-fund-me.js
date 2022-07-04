e9acf16974ea6ef4b13facf703d363fdddb03411
Sharath <sharathkrml@gmail.com>
2022-07-04 13:06:37 +0530

    added verification script using htils

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,5 +1,6 @@
 const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { getNamedAccounts, deployments, network } = require("hardhat")
+const { verify } = require("../utils/verify")
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
@@ -10,14 +11,22 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         ethUsdAggregator = await deployments.get("MockV3Aggregator")
         ethUsdPriceFeedAddress = ethUsdAggregator.address
     } else {
-        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+        ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed
     }
+    args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [ethUsdPriceFeedAddress], //priceFeed address
+        args: args, //priceFeed address
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     })
     log("FundMe deployed")
     log("------------------------------------------")
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
 }
 module.exports.tags = ["all", "fundme"]
--- hardhat.config.js
+++ hardhat.config.js
@@ -6,41 +6,29 @@ require("hardhat-gas-reporter")
 require("solidity-coverage")
 require("hardhat-deploy")
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners()
-
-    for (const account of accounts) {
-        console.log(account.address)
-    }
-})
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
     solidity: {
         compilers: [{ version: "0.8.7" }, { version: "0.6.6" }],
     },
+    defaultNetwork: "hardhat",
     networks: {
-        ropsten: {
-            url: process.env.ROPSTEN_URL || "",
-            accounts:
-                process.env.PRIVATE_KEY !== undefined
-                    ? [process.env.PRIVATE_KEY]
-                    : [],
+        rinkeby: {
+            url: process.env.RINEKBY_RPC_URL || "",
+            accounts: [process.env.PRIVATE_KEY || ""],
+            chainId: 4,
+            blockConfirmations: 6,
         },
     },
     gasReporter: {
-        enabled: process.env.REPORT_GAS !== undefined,
-        currency: "USD",
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "INR",
+        coinmarketcap: process.env.COINMARKETCAP_KEY || "",
+        token: "MATIC",
     },
     etherscan: {
-        apiKey: process.env.ETHERSCAN_API_KEY,
+        apiKey: process.env.ETHERSCAN_KEY,
     },
     namedAccounts: {
         deployer: {

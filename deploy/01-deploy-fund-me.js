a171646cc857962ab17ff55c653dddacfc7c6bc4
Sharath <sharathkrml@gmail.com>
2022-07-04 12:44:57 +0530

    Completed Deploy Script

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,16 +1,23 @@
-const { networkConfig } = require("../helper-hardhat-config")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { getNamedAccounts, deployments, network } = require("hardhat")
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
-    console.log(deploy, log)
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
-    console.log(chainId)
     // when going for localhost,we wanna use a mock
-    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [], //priceFeed address
+        args: [ethUsdPriceFeedAddress], //priceFeed address
         log: true,
     })
+    log("FundMe deployed")
+    log("------------------------------------------")
 }
+module.exports.tags = ["all", "fundme"]
